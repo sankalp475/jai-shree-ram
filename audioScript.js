7abcf46abff6d2a2d2c92cb4114107ed57b8aebd
sankalp kumar <sankalp05oct@gmail.com>
2024-01-22 18:33:59 +0530

    done

--- audioScript.js
+++ audioScript.js
@@ -4,15 +4,15 @@ let audio = document.querySelector('#background-audio');
 const allSongs = [
     {
         id: 0,
-        src: 'audio/audio1.mp3',
+        src: './audio/audio1.mp3',
     },
     {
         id: 1,
-        src: 'audio/audio2.mp3',
+        src: './audio/audio2.mp3',
     },
     {
         id: 2,
-        src: 'audio/audio3.mp3',
+        src: './audio/audio3.mp3',
     },
 ];
 
@@ -25,27 +25,26 @@ let songs = {
 };
 
 audioBtn.addEventListener('click', () => {
-    console.log('clicked')
-    if (songs.currentIndex >= 2) {
-        songs.currentIndex = 0;
-    } else {
-        songs.currentIndex += 1;
-    }
-    audio.src = allSongs[songs.currentIndex].src;
-    audio.loop = songs.loop;
-    audio.autoplay = songs.autoplay
-    audio.volume = songs.volume;
+    // console.log('clicked')
+//     if (songs.currentIndex >= allSongs.length - 1) {
+//         songs.currentIndex = 0;
+//     } else {
+//         songs.currentIndex += 1;
+//     }
+//    let src = allSongs[songs.currentIndex].src;
+//     console.log(src);
+//     audio.src = src;
     audio.play();
 })
 
-const playSong = () => {
-    audio.loop = songs.loop;
-    audio.autoplay = songs.autoplay
-    audio.volume = songs.volume;
-    // audio.play()
-    audioBtn.click();
-}
-audioBtn.click();
-window.onload = () => playSong();
+// const playSong = () => {
+//     audio.loop = songs.loop;
+//     audio.autoplay = songs.autoplay
+//     audio.volume = songs.volume;
+//     audio.play();
+//     // audioBtn.click();
+// }
+// // audioBtn.click();
+// window.onload = () => playSong();
 
 
